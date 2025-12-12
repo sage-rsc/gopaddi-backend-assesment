cd2dffc226cbed51b5eb70cf1baee029a453bbaf
James Favour <68597611+sage-rsc@users.noreply.github.com>
2025-12-12 16:00:03 +0100

    Remove unused import from CreateWalletJob

--- app/Jobs/CreateWalletJob.php
+++ app/Jobs/CreateWalletJob.php
@@ -2,7 +2,6 @@
 
 namespace App\Jobs;
 
-use App\Actions\Wallet\CreateWalletAction;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
